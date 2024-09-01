9aa2d4b7d67cbd6b69a21fa477dffe199cd1dbed
Rishi Chaturvedi <rishichaturvedi0012@gmai.com>
2024-09-01 22:57:33 +0530

    maxDuration to 10sec

--- src/app/api/mint-nft-core/route.ts
+++ src/app/api/mint-nft-core/route.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 10; // This function can run for a maximum of 5 seconds
 
 import { NextRequest, NextResponse } from 'next/server'
 import { create, mplCore } from '@metaplex-foundation/mpl-core'
